7069df616c085b903ca8284d7342352b9f4bd23d
meryemkmr <meryemkmr36@gmail.com>
2020-04-04 16:16:46 -0500

    using data to create app

--- using-data/src/App.js
+++ using-data/src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Virus from './components/Virus'
+import data from './data/virus'
 
 function App() {
+
+let dataSet = data.map((cdcData,index)=>{
+
+  return <Virus key= {index} virus= {cdcData.virus} country={cdcData.country} numViruses= {cdcData.numViruses} numDeaths= {cdcData.numDeaths}/>
+
+})
+  // before JSX return
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className ="row" style ={{padding:"30px", color:"blue", justifyContent: "center",textAlign: "center" }}>
+
+      {dataSet}
+
+    
+    {/* <Virus virus= {data[0].virus}
+        country= {data[0].country}
+        numViruses= {data[0].numViruses}
+        numDeaths= {data[0].numDeaths} />
+    <Virus virus= {data[2].virus}
+        country= {data[2].country}
+        numViruses= {data[2].numViruses}
+        numDeaths= {data[2].numDeaths} />
+    <Virus virus= {data[3].virus}
+        country= {data[3].country}
+        numViruses= {data[3].numViruses}
+        numDeaths= {data[3].numDeaths} />
+    <Virus virus= {data[4].virus}
+        country= {data[4].country}
+        numViruses= {data[4].numViruses}
+        numDeaths= {data[4].numDeaths} /> */}
+    
+  </div>
   );
 }
 
--- using-data/src/components/Virus.js
+++ using-data/src/components/Virus.js
@@ -4,6 +4,10 @@ import moduleName from '../data/virus'
 const Virus = (props) => {
   return (
     <>
+      <h2>{props.virus}</h2> <br/>
+      <h2>{props.country}</h2><br/>
+      <h2>{props.numViruses}</h2> <br/>
+      <h2>{props.numDeaths}</h2><hr/>
       
     </>
   )
