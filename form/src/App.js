d77d395e9704b34e5528c1f069894313fe23de1f
Meryem Kömür <meryemkmr36@gmail.com>
2020-04-11 14:03:24 -0500

    updated

--- form/src/App.js
+++ form/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 
 
 function App() {
@@ -8,29 +7,6 @@ function App() {
       
      
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> bcd927ff5636d6111c3d73b4402a1ef98373232a
   );
 }
 
